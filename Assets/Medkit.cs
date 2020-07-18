d15b70f164536d75e3f3c7ddc87ed18e3a9468ad
Elnuir <66650896+Elnuir@users.noreply.github.com>
2020-07-19 02:28:56 +0300

    Level 1 is expanded

---
+++ Assets/Medkit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medkit : MonoBehaviour
+{
+    public string PlayerTag = "Player";
+    public int Amount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(PlayerTag))
+        {
+            other.gameObject.GetComponent<PlayerMovement>().Health += Amount;
+            Destroy(gameObject);
+        }
+    }
+}
