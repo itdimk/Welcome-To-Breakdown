0fe52486e1ace3a6c3023fb028b72dba921ea22b
itdimk <dimk.97@ya.ru>
2020-07-20 03:39:54 +0300

    Fixed weapon movement

---
+++ Assets/WaypointMovement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WaypointMovement : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+
+    public bool Cycled = false;
+    public float Speed;
+    public float IsReachedDistance = 1.0F;
+
+    private int _currWaypointIndex;
+    private Rigidbody2D _physics;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _physics = GetComponent<Rigidbody2D>() ??
+                   throw new NullReferenceException($"Can't get {nameof(Rigidbody2D)}");
+    }
+
+
+    private void FixedUpdate()
+    {
+        MoveToCurrentWp();
+        NextWpIfRequired();
+    }
+
+    private void MoveToCurrentWp()
+    {
+        var nextWpPos = waypoints[_currWaypointIndex].position;
+        var pos = transform.position;
+        nextWpPos.z = pos.z = 0;
+        
+        var direction = nextWpPos - pos;
+        
+        float scale = Speed / direction.magnitude;
+        direction.Scale(new Vector2(scale, scale));
+        
+        _physics.velocity = direction;
+    }
+
+    private void NextWpIfRequired()
+    {  
+        var nextWpPos = waypoints[_currWaypointIndex].position;
+        var pos = transform.position;
+
+        nextWpPos.z = pos.z = 0;
+        
+        if ((nextWpPos - pos).magnitude <= IsReachedDistance)
+        {
+            if (_currWaypointIndex < waypoints.Count - 1)
+                ++_currWaypointIndex;
+            else if (Cycled)
+                _currWaypointIndex = 0;
+        }
+    }
+}
---
+++ Assets/WeaponMovement.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UnityEngine;
+
+public class WeaponMovement : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+    public float maxAngleUp = 90.0F;
+    public float maxAngleDown = 90.0F;
+    public float offset = 0.0F;
+    
+    public KeyCode MoveRightKey;
+    public KeyCode MoveLeftKey;
+
+    private bool _mirror = false;
+    
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotationZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (Input.GetKey(MoveRightKey))
+            _mirror = false;
+
+        if (Input.GetKey(MoveLeftKey))
+            _mirror = transform;
+        
+        if(_mirror)
+            ApplyRotationMirror(rotationZ);
+        else
+            ApplyRotation(rotationZ);
+    }
+
+    
+    private void ApplyRotationMirror(float rotationZ)
+    {
+        Quaternion after = Quaternion.Euler(0f, 0f, LimitRotationMirror(rotationZ) + offset);
+        
+        transform.rotation = new Quaternion(after.x, after.y, after.z, after.w);
+        transform.Rotate(180, 0, 0);
+       
+    }
+    
+    private void ApplyRotation(float rotationZ)
+    {
+        Quaternion after = Quaternion.Euler(0f, 0f, LimitRotation(rotationZ) + offset);
+
+        transform.rotation = new Quaternion(after.x, after.y, after.z, after.w);
+    }
+
+    private float LimitRotation(float rotation)
+    {
+        if (rotation > maxAngleUp)
+            return maxAngleUp;
+
+        if (rotation < -maxAngleDown)
+            return -maxAngleDown;
+
+        return rotation;
+    }
+
+    private float LimitRotationMirror(float rotation)
+    {
+        float maxAngleUp_ = 180 - maxAngleUp;
+        float maxAngleDown_ = -180 + maxAngleDown;
+
+
+        if (rotation < maxAngleUp_ && rotation > maxAngleDown_)
+        {
+            float deltaUp = Mathf.Abs(maxAngleUp_ - rotation);
+            float deltaDown = Mathf.Abs(maxAngleDown_ - rotation);
+
+            rotation = deltaUp < deltaDown ? maxAngleUp_ : maxAngleDown_;
+        }
+
+        return rotation;
+    }
+}
