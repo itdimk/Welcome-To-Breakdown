b10e5978faaea422ff9dd9c1e455d7c00db93c6d
Elnuir <66650896+Elnuir@users.noreply.github.com>
2020-07-26 01:26:00 +0300

    Revert "level4"
    This reverts commit afe59f90710ff30f6cfb64e11b3fd4611260b2bf.

--- Assets/Activator.cs
+++ Assets/Activator.cs
@@ -4,31 +4,26 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-public enum Hook
-{
-    None,
-    TriggerEnter,
-    TriggerStay,
-    TriggerExit,
-    ButtonDown,
-    ButtonUp,
-    Time
-}
-
-
 public class Activator : MonoBehaviour
 {
     public List<GameObject> TargetObjects = new List<GameObject>();
     public List<MonoBehaviour> TargetScripts = new List<MonoBehaviour>();
     public List<Component> TargetComponents = new List<Component>();
-    
-    public Hook ActivateBy;
-    public Hook DeactivateBy;
 
+
+    public bool SetActiveOnTriggerEnter;
+    public bool SetActiveOnTriggerStay;
+    public bool SetActiveOnTriggerExit;
     public List<string> WhoIsTriggered = new List<string> {"Player"};
 
-    public float TargetTime;
-    public string TargetButton;
+    public bool SetActiveInTime;
+    public float Time;
+
+    public bool SetActiveOnKeyDown;
+    public bool SetActiveOnKeyUp;
+    public KeyCode Key;
+
+    public bool SetActiveTo;
 
     private float startTick;
 
@@ -37,72 +32,57 @@ public class Activator : MonoBehaviour
 
     private void Start()
     {
-        startTick = Time.time;
+        startTick = UnityEngine.Time.time;
     }
 
     private void FixedUpdate()
     {
-        bool timeIsUp = Time.time >= startTick + TargetTime;
-
-        if (ActivateBy == Hook.Time && timeIsUp)
-            SetAll(true);
-        else if (DeactivateBy == Hook.Time && timeIsUp)
-            SetAll(false);
-
-        if (!string.IsNullOrWhiteSpace(TargetButton))
-        {
-            bool buttonPressed = Input.GetButton(TargetButton);
-
-            if (buttonPressed)
-            {
-                if (ActivateBy == Hook.ButtonDown)
-                    SetAll(true);
-                if (DeactivateBy == Hook.ButtonDown)
-                    SetAll(false);
-            }
-            else
-            {
-                if (ActivateBy == Hook.ButtonUp)
-                    SetAll(true);
-                if (DeactivateBy == Hook.ButtonUp)
-                    SetAll(false);
-            }
-        }
+        if (SetActiveInTime && UnityEngine.Time.time >= startTick + Time)
+            SetAll();
+        else if (SetActiveOnKeyDown && Input.GetKey(Key))
+            SetAll();
+        else if (SetActiveOnKeyUp && !Input.GetKey(Key))
+            SetAll();
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
-        => SetAllByTriggerIfRequired(other.tag, Hook.TriggerEnter);
-
-
-    private void OnTriggerExit2D(Collider2D other)
-        => SetAllByTriggerIfRequired(other.tag, Hook.TriggerExit);
-
-    private void OnTriggerStay2D(Collider2D other)
-        => SetAllByTriggerIfRequired(other.tag, Hook.TriggerStay);
-
-    private void SetAllByTriggerIfRequired(string tag, Hook hook)
     {
-        if (WhoIsTriggered.Contains(tag))
+        if (WhoIsTriggered.Contains(other.gameObject.tag))
         {
-            if (ActivateBy == hook)
-                SetAll(true);
-            else if (DeactivateBy == hook)
-                SetAll(false);
+            if (SetActiveOnTriggerEnter)
+                SetAll();
         }
     }
 
-    private void SetAll(bool value)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        TargetObjects.ForEach(o => { o.SetActive(value); });
+        if (WhoIsTriggered.Contains(other.gameObject.tag))
+        {
+            if (SetActiveOnTriggerExit)
+                SetAll();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (WhoIsTriggered.Contains(other.gameObject.tag))
+        {
+            if (SetActiveOnTriggerStay)
+                SetAll();
+        }
+    }
+
+    private void SetAll()
+    {
+        TargetObjects.ForEach(o => { o.SetActive(SetActiveTo); });
         TargetComponents.Concat(TargetScripts).ToList().ForEach(c =>
         {
             Type type = c.GetType();
             if (!_cachedType.ContainsKey(type))
                 _cachedType.Add(type, type.GetProperty(nameof(enabled)));
 
-            if ((bool) _cachedType[type].GetValue(c) != value)
-                _cachedType[type].SetValue(c, value);
+            if ((bool) _cachedType[type].GetValue(c) != SetActiveTo)
+                _cachedType[type].SetValue(c, SetActiveTo);
         });
     }
 }
