d8cab9e12029918188ae282fdffc330c9c42cd30
itdimk <dimk.97@ya.ru>
2020-07-26 23:04:00 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/itdimk/dumb-game""
    This reverts commit 9855645c3036aa925b7c246525d024284fd114c3.

---
+++ Assets/NewScripts2/JointCord.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointCord : MonoBehaviour
+{
+    public Transform ConnectedObject;
+    private LineRenderer _line;
+
+    public Material Material;
+    public float Width = 1.0F;
+
+    public Joint2D Joint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _line.material = Material;
+        _line.startWidth = Width;
+        _line.endWidth = Width;
+    }
+
+    private void Awake()
+    {
+        _line = gameObject.AddComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (_line != null)
+        {
+            _line.SetPositions(new[] {transform.position, ConnectedObject.transform.position});
+
+            if (!TryGetComponent<Joint2D>(out _))
+            {
+                Destroy(_line);
+                Destroy(this);
+            }
+        }
+    }
+}
