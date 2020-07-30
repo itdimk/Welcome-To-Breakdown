5b88340129f0967c41de506df3e74e248b35a33d
Elnuir <66650896+Elnuir@users.noreply.github.com>
2020-07-31 02:51:26 +0300

    xx
    xx

---
+++ Assets/Dialogue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public GameObject continueButton;
+
+    private void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    private void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueButton.SetActive(false);
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+        }
+    }
+}
