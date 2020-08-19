ea0f4ccf76f929e82ecd436136e392aee4378d42
itdimk <dimk.97@ya.ru>
2020-08-19 22:21:08 +0300

    End level dialog destroy fixes

--- Assets/GoodScriptsCollection/AnimatorParameterMapperD.cs
+++ Assets/GoodScriptsCollection/AnimatorParameterMapperD.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-
 
 public class AnimatorParameterMapperD : MonoBehaviour
 {
